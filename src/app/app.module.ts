cf770a06141f5f5b2de795acd299cdc65bd7545d
Mailen Havela <mailen.havela@gmail.com>
2017-08-26 14:53:10 -0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here. The ng module for forms.
 
 import { AppComponent }  from './app.component';
 
+/**
+ * The @NgModule metadata's imports array, which contains the list of external modules that the app uses.
+ * NgModule contiene un array 'imports' que lista los modulos exteernos que usa la app.
+ */
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ 
+    BrowserModule,
+    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+  ],
   declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  
 })
 export class AppModule { }
